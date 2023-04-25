e5ab872f515de9cf3dc4fcf7cb4966fdd8cebc48
franklinegit <125601790+franklinegit@users.noreply.github.com>
2023-04-25 21:01:44 +0000

    main.h file

--- main.h
+++ main.h
@@ -3,8 +3,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdarg.h>
+#include <math.h>
 
 int _printf(const char *format, ...);
+int _putchar(int c);
+void print_int(int num);
+void print_unsigned_decimal(unsigned int num);
+void print_octal(unsigned int num);
+void print_hex(unsigned int num);
+void printHex(unsigned int num);
 
 #endif
